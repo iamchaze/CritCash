54be76ba7e82aa584a35d185ed0a46dbb2aa86f1
Viraj Kale <virajkale9604@gmail.com>
2025-08-24 03:08:56 +0530

    feat: enhance History page layout and styling for better transaction display

--- frontend/src/pages/History.jsx
+++ frontend/src/pages/History.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import titleCase from "../utils/titleCase";
+import DesktopSideBar from "../components/DesktopSideBar";
 
 const History = () => {
   const [transactions, setTransactions] = useState([]);
@@ -31,41 +33,93 @@ const History = () => {
 
   return (
     <>
-      <h1>Transaction History</h1>
-      {loading ? (
-        <p>Loading...</p>
-      ) : error ? (
-        <p>{error}</p>
-      ) : transactions.length === 0 ? (
-        <p>No transactions found.</p>
-      ) : (
-        <div>
-          {transactions.map((transaction) => {
-            return (
-              <div
-                style={{ marginBottom: "20px", cursor: "pointer" }}
-                key={transaction.id}
-                onClick={() =>
-                  navigate("/transactiondetails", { state: { transaction } })
-                }
-              >
-                <p>
-                  {transaction.type === "sent"
-                    ? `Money sent to ${transaction.receiverDetails.firstName} ${transaction.receiverDetails.lastName}`
-                    : transaction.type === "received"
-                    ? `Money received from ${transaction.senderDetails.firstName} ${transaction.senderDetails.lastName}`
-                    : "error"}
-                </p>
-                <pre>
-                  Amount Rs.{transaction.amount} Date:{transaction.date} Time:
-                  {transaction.time}
-                </pre>
-                <hr className="border-0 h-[2px]" />
-              </div>
-            );
-          })}
+      <div className="lg:flex h-screen">
+        <DesktopSideBar />
+        <div className="bg-primary h-screen flex-1">
+        <div className=" bg-accent2 text-center p-5 lg:p-7 text-2xl lg:text-3xl font-bold font-[REM]">
+          <h1>All Transactions</h1>
         </div>
-      )}
+
+        {loading ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p>{error}</p>
+        ) : transactions.length === 0 ? (
+          <div className="w-full my-30 m-auto font-[REM] font-semibold text-lg text-center">
+            <p>No transactions found.</p>
+          </div>
+        ) : (
+          <div>
+            {transactions.map((transaction) => {
+              return (
+                <div
+                  className="cursor-pointer bg-accent6 p-5 m-3 rounded-xl shadow-md"
+                  key={transaction.id}
+                  onClick={() =>
+                    navigate("/transactiondetails", { state: { transaction } })
+                  }
+                >
+                  <div className="flex flex-row justify-between gap-2 items-center">
+                    <div className="flex flex-row items-center justify-between gap-3">
+                      <img
+                        src="/images/person1.jpg"
+                        alt="profile image"
+                        className="inline w-15 h-15 object-cover rounded-full"
+                      />
+                      <div className="font-semibold text-lg">
+                        {" "}
+                        {transaction.type === "sent"
+                          ? `${titleCase(
+                              transaction.receiverDetails.firstName
+                            )} ${titleCase(
+                              transaction.receiverDetails.lastName
+                            )}`
+                          : transaction.type === "received"
+                          ? `${titleCase(
+                              transaction.senderDetails.firstName
+                            )} ${titleCase(transaction.senderDetails.lastName)}`
+                          : "error"}
+                      </div>
+                    </div>
+                    <div
+                      className={`font-bold py-1 px-4 rounded-full 
+                                  ${
+                                    transaction.type === "sent"
+                                      ? "bg-red-200 text-red-800"
+                                      : ""
+                                  } 
+                                  ${
+                                    transaction.type === "received"
+                                      ? "bg-green-200 text-green-800"
+                                      : ""
+                                  } 
+                                  ${
+                                    transaction.type === "failed"
+                                      ? "bg-gray-200 text-gray-800"
+                                      : ""
+                                  }`}
+                    >
+                      {transaction.type}
+                    </div>
+                    <div className="flex flex-col items-end justify-between gap-1 font-[REM]">
+                      <div className=" text-xl font-semibold">
+                        Rs.{transaction.amount}
+                      </div>
+                      <div>
+                        {transaction.date.slice(0, 10)}
+                        {", "}
+                        {transaction.time}
+                      </div>
+                    </div>
+                  </div>
+                  <hr className="border-0 h-[2px]" />
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
+      </div>
     </>
   );
 };
