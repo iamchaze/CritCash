a2e60a770a59dd7bf56b2a9dd416331637fa3bb6
Viraj Kale <virajkale9604@gmail.com>
2026-01-13 01:59:38 +0530

    name case fix authmiddleware

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,36 +1,3 @@
-require("dotenv").config();
-const express = require("express");
-const mainRouter = require("./routes/mainRouter");
-const cors = require("cors");
-const cookieParser = require('cookie-parser');
-
-const app = express();
-
-app.use(cors({
-    origin: process.env.CLIENT_URL, // frontend URL
-    credentials: true
-}));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-
-
-app.use('/api/v1', mainRouter);
-
-
-
-app.get("/", (req, res) => {
-    res.send("<h1>Welcome to Paymate</h1>");
-})
-
-
-app.listen(port = 5000, () => {
-    console.log(`http://localhost:${port}`);
-});
-
-
-
-
 const jwt = require('jsonwebtoken')
 
 const authMiddleware = (req, res, next) => {
