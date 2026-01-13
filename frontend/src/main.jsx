53fd45d1407f7073789b64f65939de9817300232
Viraj Kale <virajkale9604@gmail.com>
2026-01-13 19:25:05 +0530

    fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,6 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <h1 style={{ color: "white" }}>CRIT CASH IS LOADING</h1>
+)
