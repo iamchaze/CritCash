89a546a72d613300fe149d81bfd02fa2e06e6733
Viraj Kale <virajkale9604@gmail.com>
2026-01-13 11:49:15 +0530

    titleCase name fix part 1

--- frontend/src/utils/titleCase.js
+++
@@ -1,7 +0,0 @@
-export default function titleCase(str){
-    return str
-        .toLowerCase()
-        .split(' ')
-        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-        .join(' ');
-}
